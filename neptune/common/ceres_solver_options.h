82f76efc458b7bcfe84245f864ea686a0e10634a
Yaping <yaping.liu@anker.com>
2022-08-24 11:23:23 +0800

    test

---
+++ neptune/common/ceres_solver_options.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_CERES_SOLVER_OPTIONS_H_
+#define COMMON_CERES_SOLVER_OPTIONS_H_
+
+#include "ceres/ceres.h"
+#include "neptune/common/lua_parameter_dictionary.h"
+
+namespace neptune {
+namespace common {
+
+// proto::CeresSolverOptions CreateCeresSolverOptionsProto(
+//     common::LuaParameterDictionary* parameter_dictionary);
+
+// ceres::Solver::Options CreateCeresSolverOptions(
+//     const proto::CeresSolverOptions& proto);
+
+}  // namespace common
+}  // namespace neptune
+
+#endif  // CARTOGRAPHER_COMMON_CERES_SOLVER_OPTIONS_H_
